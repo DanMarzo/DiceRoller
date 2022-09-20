69881ddeca20c19aacfbd37b6ab24406a8ec6216
DanMarzo <marzogildan@gmail.com>
2022-09-20 20:38:32 -0300

    TextView assumindo numeros randomicos

--- app/src/main/java/com/example/atividadepm/MainActivity.kt
+++ app/src/main/java/com/example/atividadepm/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.atividadepm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.buttonNumber)
+        rollButton.setOnClickListener{ rollDice() }
     }
+
+    fun rollDice()
+    {
+        val randomint = (1..6).random().toString()
+        val resultText: TextView = findViewById(R.id.resultadoRandom)
+
+        resultText.text = randomint
+    }
+
 }
