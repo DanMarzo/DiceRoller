ca1086b6f480780e7007a97735ed5ca308bd2289
DanMarzo <marzogildan@gmail.com>
2022-09-27 17:54:17 -0300

    Create folder binding

---
+++ ComBinding/app/src/main/java/com/example/atividadepm/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.atividadepm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       // private lateinit var appBarConfiguration: AppBarConfiguration
+        //private lateinit var binding: ActivityMainBinding
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val rollButton: Button = findViewById(R.id.buttonNumber)
+//        rollButton.setOnClickListener{ rollDice() }
+    }
+
+    fun rollDice()
+    {
+        val randomint = (1..6).random().toString()
+        val resultText: TextView = findViewById(R.id.resultadoRandom)
+
+        resultText.text = randomint
+    }
+
+}
