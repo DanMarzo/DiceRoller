7315e645dcb44dcb39473580d000cb8fbdb71342
DanMarzo <marzogildan@gmail.com>
2022-10-18 21:32:56 -0300

    Correção do binding

--- ComBinding/app/src/main/java/com/example/atividadepm/MainActivity.kt
+++ ComBinding/app/src/main/java/com/example/atividadepm/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.atividadepm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageSwitcher
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.atividadepm.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -10,21 +12,36 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.buttonNumber.setOnClickListener{rollDice()}
-
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding.buttonNumber.setOnClickListener{rollDice()}
+        binding.buttonNumber.setOnClickListener{ rollDice() }
     }
 
-    fun rollDice()
+    private fun rollDice() {
+        val dice = Dice(6);
+        val diceRoll = dice.rool();
+        val diceImage: ImageView = binding.resultadoRandom;
+        diceImage.setImageResource(R.drawable.dice_2);
+    }
+
+    /*fun rollDice()
     {
         val resultText: TextView = binding.resultadoRandom
         val gerarNumero = binding.numberData.text.toString().toInt()
         val saidaRandom = (1..gerarNumero).random()
         resultText.text = saidaRandom.toString()
+    }*/
+
+    class Dice (val numSides: Int) {
+        fun rool(): Int {
+            return (1..numSides).random();
+        }
     }
 
 }
